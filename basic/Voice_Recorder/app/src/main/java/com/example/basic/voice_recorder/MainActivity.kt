18e27011e89bf850a59235c450b33a2ae0030a04
seoyoon528 <seoyoon528@gmail.com>
2022-01-23 23:47:04 +0900

    녹음기_ 버튼 활성화

--- basic/Voice_Recorder/app/src/main/java/com/example/basic/voice_recorder/MainActivity.kt
+++ basic/Voice_Recorder/app/src/main/java/com/example/basic/voice_recorder/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.basic.voice_recorder
 
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+    private val resetButton: Button by lazy {
+        findViewById(R.id.resetButton)
+    }
 
     private val recordButton: RecordButton by lazy {
         findViewById(R.id.recordButton)
     }
 
-    // TODO Manifest.permission 오류 고치기
-//    private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private val requiredPermissions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
 
+    private val recordingFilePath: String by lazy {        // recording file을 저장할 수 있는 path 지정
+        "${externalCacheDir?.absolutePath}/recording.3gp"
+    }
+
+    private var recorder: MediaRecorder? = null
+    private var player: MediaPlayer? = null
     private var state = State.BEFORE_RECORDING
+        set(value) {
+            field = value   // 실제 property에 새로 들어온 값 value를 넣어줌
+            resetButton.isEnabled = ( value == State.AFTER_RECORDING ) || ( value ==  State.ON_PLAYING )    // reset 버튼 활성화
+            recordButton.updateIconWithState(value)    // 버튼에 새로 들어온 value를 넣어주며 버튼 update
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,30 +40,89 @@ class MainActivity : AppCompatActivity() {
 
         requestAudioPermission()
         initViews()
+        bindViews()
+        initVariables()
     }
 
-    override fun onRequestPermissionsResult(        // 권한이 부여되어있는지 확인
+    override fun onRequestPermissionsResult(        // 녹음 권한이 부여되어있는지 확인
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        val audioRecordPermissionGranted =
+        val audioRecordPermissionGranted =      // 녹음 권한이 부여된 경우
             requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
             grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
 
-        if (!audioRecordPermissionGranted) {        // 권한이 부여되지 않았다면
-            finish()        // 종료
+        if (!audioRecordPermissionGranted) {        // 녹음 권한이 부여되지 않은 경우
+            finish()        // 앱 종료
         }
     }
 
     private fun requestAudioPermission() {
-//        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
     }
 
     private fun initViews() {
+        resetButton.setOnClickListener {
+            stopPlaying()
+            state = State.BEFORE_RECORDING
+        }
         recordButton.updateIconWithState(state)
+    }
+
+    private fun bindViews() {
+        recordButton.setOnClickListener {
+            when(state) {
+                State.BEFORE_RECORDING -> { startRecording() }
+                State.ON_RECORDING -> { stopRecording() }
+                State.AFTER_RECORDING -> {startPlaying()}
+                State.ON_PLAYING -> {stopPlaying()}
+            }
+        }
+    }
+
+    private fun initVariables() {
+        state = State.BEFORE_RECORDING
+    }
+
+    private fun startRecording() {      // recorder 초기화
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)       // AudioSource :: MIC로 set
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            setOutputFile(recordingFilePath)     // 용량 확보를 위해 외부 storage에 접근해 저장
+            prepare()
+        }
+        recorder?.start()   // 녹음 시작
+        state = State.ON_RECORDING
+    }
+
+    private fun stopRecording() {
+        recorder?.run {
+            stop()
+            release()   // 메모리 해제
+        }
+        recorder = null
+        state = State.AFTER_RECORDING
+    }
+
+    private fun startPlaying() {
+        player = MediaPlayer()
+            .apply {
+                setDataSource(recordingFilePath)
+                prepare()
+            }
+        player?.start()
+        state = State.ON_PLAYING
+    }
+
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+        state = State.AFTER_RECORDING
+
     }
 
     companion object {      // request code
