a07d223612b3e8240a1c196888e1fa4915a0a187
seoyoon528 <seoyoon528@gmail.com>
2022-01-29 21:52:03 +0900

    오늘의 명언

--- intermediate/Quote_of_the_Today/app/src/main/java/com/example/intermediate/quote_of_the_today/MainActivity.kt
+++ intermediate/Quote_of_the_Today/app/src/main/java/com/example/intermediate/quote_of_the_today/MainActivity.kt
@@ -2,23 +2,48 @@ package com.example.intermediate.quote_of_the_today
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import androidx.viewpager2.widget.ViewPager2
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import org.json.JSONArray
 import org.json.JSONObject
+import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
+
     private val viewPager: ViewPager2 by lazy {
         findViewById(R.id.viewPager)
+    }
+
+    private val progressBar: ProgressBar by lazy {
+        findViewById(R.id.progressBar)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initViews()
         initData()
+    }
+
+    private fun initViews() {
+        viewPager.setPageTransformer { page, position ->        // PageTransformer :: ViewPager의 Custom Swipe 효과
+            when {
+                position.absoluteValue >= 1.0F -> {         // position의 절댓값이 1 이상인 경우(= 현재 페이지의 앞, 뒤 페이지가 아닌 경우)
+                    page.alpha = 0F     //  완전히 투명하게 함
+                }
+                position == 0F -> {
+                    page.alpha = 1F     // 현재 페이지는 전부 보이게 함(=불투명)
+                }
+                else -> {
+                    page.alpha = 1F - 2 * position.absoluteValue        // 현재 페이지에서 멀어질수록 투명하게 함 (기울기 값을 가파르게 하기 위해 x2)
+                }
+            }
+        }
     }
 
     private fun initData() {
@@ -30,6 +55,8 @@ class MainActivity : AppCompatActivity() {
             }
         )
         remoteConfig.fetchAndActivate().addOnCompleteListener {
+            progressBar.visibility = View.GONE
+
             if(it.isSuccessful) {       // Fetch & Activate 작업이 성공했다면
                 val quotes = parseQuotesJson(remoteConfig.getString("quotes"))        // quotes & isNameRevealed flag 가져와서
                 val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
@@ -59,9 +86,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun displayQuotesPager(quotes: List<Quote>, isNameRevealed: Boolean) {
-        viewPager.adapter = QuotesPagerAdapter(
+        val adapter = QuotesPagerAdapter(
             quotes,
             isNameRevealed
         )
+
+        viewPager.adapter = adapter
+        viewPager.setCurrentItem(adapter.itemCount / 2, false)      // 양쪽으로 무한 Swipe 할 수 있도록 전체 item size의 중간 position에 quote를 위치하게 함
     }
 }
--- intermediate/Quote_of_the_Today/app/src/main/java/com/example/intermediate/quote_of_the_today/QuotesPagerAdapter.kt
+++ intermediate/Quote_of_the_Today/app/src/main/java/com/example/intermediate/quote_of_the_today/QuotesPagerAdapter.kt
@@ -18,20 +18,22 @@ class QuotesPagerAdapter(
                 )
 
     override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
-        holder.bind(quotes[position], isNameRevealed)   // 해당 position의 quote를 전달하며 bind 호출
+        val actualPosition = position % quotes.size     // 마지막 quote의 다음을 첫번째 quote로 설정하여 무한 Swipe
+
+        holder.bind(quotes[actualPosition], isNameRevealed)   // 해당 position의 quote를 전달하며 bind 호출
     }
 
-    override fun getItemCount() = quotes.size
+    override fun getItemCount() = Int.MAX_VALUE     // 페이지를 무한대로 Swipe 할 수 있게하기 위해 item 개수를 크게 설정
 
     class QuoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val quoteTextView: TextView = itemView.findViewById(R.id.quoteTextView)
         private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
 
         fun bind(quote: Quote, isNameRevealed: Boolean) {
-            quoteTextView.text = quote.quote
+            quoteTextView.text = "\" ${quote.quote}\""          // " 명언 " Format
 
             if(isNameRevealed) {
-                nameTextView.text = quote.name
+                nameTextView.text  = "- ${quote.name}"
                 nameTextView.visibility = View.VISIBLE
             } else {
                 nameTextView.visibility = View.GONE
