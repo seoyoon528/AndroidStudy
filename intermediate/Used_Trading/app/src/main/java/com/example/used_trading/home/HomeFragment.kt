6bfbc4a20a48b4d87357be662679643fc0b22339
seoyoon528 <seoyoon528@gmail.com>
2022-02-22 15:36:40 +0900

    [Feat] HomeFragment에 RecyclerView 연결

--- intermediate/Used_Trading/app/src/main/java/com/example/used_trading/home/HomeFragment.kt
+++ intermediate/Used_Trading/app/src/main/java/com/example/used_trading/home/HomeFragment.kt
@@ -1,7 +1,26 @@
 package com.example.used_trading.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.used_trading.R
+import com.example.used_trading.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+
+    private var binding: FragmentHomeBinding? = null
+    private lateinit var articleAdapter: ArticleAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)        // null이 될 수 없도록 지역변수로 binding 선언
+        binding = fragmentHomeBinding
+
+        articleAdapter = ArticleAdapter()
+
+        fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
+        fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
+    }
 }
