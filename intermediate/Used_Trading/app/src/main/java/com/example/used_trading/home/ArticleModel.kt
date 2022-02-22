c0bbedabb9ad1581cd287bf7a9ca7f55a1836951
seoyoon528 <seoyoon528@gmail.com>
2022-02-22 20:23:36 +0900

    [Feat] Firebase에서 data 가져와 RecyclerView에 띄우기

--- intermediate/Used_Trading/app/src/main/java/com/example/used_trading/home/ArticleModel.kt
+++ intermediate/Used_Trading/app/src/main/java/com/example/used_trading/home/ArticleModel.kt
@@ -6,4 +6,8 @@ data class ArticleModel(
     val createdAt: Long,
     val price: String,
     val imageUrl: String
-)
+) {
+        //  Firebase Realtime database에 Model class를 그대로 사용하기 위해서는 빈 생성자 필요
+        constructor(): this("", "", 0, "", "")
+
+}
--- intermediate/Used_Trading/app/src/main/java/com/example/used_trading/home/HomeFragment.kt
+++ intermediate/Used_Trading/app/src/main/java/com/example/used_trading/home/HomeFragment.kt
@@ -4,13 +4,47 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.used_trading.DBKey.Companion.DB_ARTICLES
 import com.example.used_trading.R
 import com.example.used_trading.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
 
-    private var binding: FragmentHomeBinding? = null
     private lateinit var articleAdapter: ArticleAdapter
+    private lateinit var articleDB: DatabaseReference
+
+    private var binding: FragmentHomeBinding? = null
+    private val auth : FirebaseAuth by lazy {
+        Firebase.auth
+    }
+
+    private val articleList = mutableListOf<ArticleModel>()
+    private val listener = object: ChildEventListener {
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+
+            val articleModel = snapshot.getValue(ArticleModel::class.java)      //  Model class 자체로 DB에 데이터를 올리고 내려받음
+            articleModel ?: return      // ArticleModel이 null이라면 return
+
+            articleList.add(articleModel)       //  null이 아니라면
+            articleAdapter.submitList(articleList)      //  articleAdapter에 articleList를 넣어줌
+        }
+
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+
+        override fun onChildRemoved(snapshot: DataSnapshot) {}
+
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+
+        override fun onCancelled(error: DatabaseError) {}
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -20,7 +54,25 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
 
         articleAdapter = ArticleAdapter()
 
+        articleList.clear()     // List 초기화
         fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
         fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
+
+        articleDB = Firebase.database.reference.child(DB_ARTICLES)
+
+        articleDB.addChildEventListener(listener)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        articleAdapter.notifyDataSetChanged()       //  view를 다시 그림
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        articleDB.removeEventListener(listener)
     }
 }
