b21e8641493f7fe81985bc75033a1baa56edba52
seoyoon528 <seoyoon528@gmail.com>
2022-02-26 23:56:37 +0900

    [Feat] Use Naver Map API

--- intermediate/Airbnb/app/src/main/java/com/example/airbnb/MainActivity.kt
+++ intermediate/Airbnb/app/src/main/java/com/example/airbnb/MainActivity.kt
@@ -1,11 +1,106 @@
 package com.example.airbnb
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private val mapView: MapView by lazy {
+        findViewById(R.id.mapView)
+    }
+
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mapView.onCreate(savedInstanceState)
+
+        // naver Map 객체 가져오기
+        mapView.getMapAsync (this)      //  callback method :: OnMapReadyCallback
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+
+        naverMap.maxZoom = 18.0
+        naverMap.minZoom = 10.0
+
+        //  지정한 위도, 경도 위치로 Camera 위치 update
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.52862006299251, 126.9330290239728))
+        naverMap.moveCamera(cameraUpdate)
+
+        // 현위치 설정
+        val uiSetting = naverMap.uiSettings
+        uiSetting.isLocationButtonEnabled = true        //  현위치 버튼 활성화
+
+        // 현재 위치 권한 받기
+        locationSource = FusedLocationSource(this@MainActivity, LOCATION_PERMISSION_REQUEST_CODE )
+        naverMap.locationSource = locationSource
+
+        // 지정한 위도, 경도 위치에 핀(마커) 꽂기
+        val marker = Marker()
+        marker.position = LatLng(37.52628019035669, 126.92851218398253)
+        marker.map = naverMap
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return
+        }
+
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated) {      //  권한이 거부되었음을 네이버에 알림
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
